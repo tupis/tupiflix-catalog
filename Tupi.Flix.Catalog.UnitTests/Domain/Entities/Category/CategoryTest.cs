150ab91e25c07f8621d19098f9ec767a1954799e
João Tupinambá <joaoh.tupinamba@gmail.com>
2024-09-18 10:38:36 -0300

    test: dinamic value active status

--- Tupi.Flix.Catalog.UnitTests/Domain/Entities/Category/CategoryTest.cs
+++ Tupi.Flix.Catalog.UnitTests/Domain/Entities/Category/CategoryTest.cs
@@ -27,5 +27,31 @@ namespace Tupi.Flix.Catalog.UnitTests.Domain.Entities.Category
             Assert.True(category.CreatedAt < dateTimeAfter);
             Assert.True(category.IsActive);
         }
+
+        [Theory(DisplayName = nameof(InstantiateWithIsActiveStatus))]
+        [Trait("Domain", "Category - Aggregates")]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void InstantiateWithIsActiveStatus(bool isActive)
+        {
+            var validData = new
+            {
+                Name = "category name",
+                Description = "category description",
+            };
+
+            var dateTimeBefore = DateTime.Now;
+            var category = new DomainCategoryEntity(validData.Name, validData.Description, isActive);
+            var dateTimeAfter = DateTime.Now;
+
+            Assert.NotNull(category);
+            Assert.Equal(validData.Name, category.Name);
+            Assert.Equal(validData.Description, category.Description);
+            Assert.NotEqual(default(Guid), category.Id);
+            Assert.NotEqual(default(DateTime), category.CreatedAt);
+            Assert.True(category.CreatedAt > dateTimeBefore);
+            Assert.True(category.CreatedAt < dateTimeAfter);
+            Assert.Equal(category.IsActive, isActive);
+        }
     }
 }
