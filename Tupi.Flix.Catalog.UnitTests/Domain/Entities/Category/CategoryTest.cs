2315712550d5e64d82ae263b17c49acd145c61c2
João Tupinambá <joaoh.tupinamba@gmail.com>
2024-09-18 10:24:04 -0300

    teste: create instantiate category

---
+++ Tupi.Flix.Catalog.UnitTests/Domain/Entities/Category/CategoryTest.cs
@@ -0,0 +1,30 @@
+﻿using DomainCategoryEntity = Tupi.Flix.Catalog.Domain.Entities.Category;
+
+namespace Tupi.Flix.Catalog.UnitTests.Domain.Entities.Category
+{
+    public class CategoryTest
+    {
+        [Fact(DisplayName = nameof(Instantiate))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void Instantiate()
+        {
+            var validData = new
+            {
+                Name = "category name",
+                Description = "category description",
+            };
+
+            var dateTimeBefore = DateTime.Now;
+            var category = new DomainCategoryEntity(validData.Name, validData.Description);
+            var dateTimeAfter = DateTime.Now;
+
+            Assert.NotNull(category);
+            Assert.Equal(validData.Name, category.Name);
+            Assert.Equal(validData.Description, category.Description);
+            Assert.NotEqual(default(Guid), category.Id);
+            Assert.NotEqual(default(DateTime), category.CreatedAt);
+            Assert.True(category.CreatedAt > dateTimeBefore);
+            Assert.True(category.CreatedAt < dateTimeAfter);
+        }
+    }
+}
