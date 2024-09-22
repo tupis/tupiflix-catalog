33d83db51048987e93843caa7e15e5906e499296
João Tupinambá <joaoh.tupinamba@gmail.com>
2024-09-22 15:38:28 -0300

    feat: autogenerate mock instantiate

---
+++ Tupi.Flix.Catalog.UnitTests/Domain/Entities/Category/CategoryTestMock.cs
@@ -0,0 +1,12 @@
+﻿using DomainCategoryEntity = Tupi.Flix.Catalog.Domain.Entities.Category;
+
+namespace Tupi.Flix.Catalog.UnitTests.Domain.Entities.Category
+{
+    public class CategoryTestMock
+    {
+        public static DomainCategoryEntity Create()
+        {
+            return new DomainCategoryEntity("Category Name", "DescritionName");
+        }
+    }
+}
