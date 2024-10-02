f79305057dbef67b75a07571fb6e86b4d354a96b
João Tupinambá <joaoh.tupinamba@gmail.com>
2024-10-02 11:08:28 -0300

    feat: base mock

---
+++ Tupi.Flix.Catalog.UnitTests/Domain/Common/BaseMock.cs
@@ -0,0 +1,11 @@
+﻿using Bogus;
+
+namespace Tupi.Flix.Catalog.UnitTests.Domain.Common
+{
+    public abstract class BaseMock
+    {
+        protected BaseMock() => Faker = new Faker("pt_BR");
+
+        public Faker Faker {  get; set; }
+    }
+}
