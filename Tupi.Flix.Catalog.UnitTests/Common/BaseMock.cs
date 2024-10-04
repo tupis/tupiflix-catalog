4c0d5ae58286d11261ff58d311ade7233749cac7
João Tupinambá <joaoh.tupinamba@gmail.com>
2024-10-04 01:02:01 -0300

    style: move out folder

--- Tupi.Flix.Catalog.UnitTests/Common/BaseMock.cs
+++ Tupi.Flix.Catalog.UnitTests/Common/BaseMock.cs
@@ -1,11 +1,11 @@
 ﻿using Bogus;
 
-namespace Tupi.Flix.Catalog.UnitTests.Domain.Common
+namespace Tupi.Flix.Catalog.UnitTests.Common
 {
     public abstract class BaseMock
     {
         protected BaseMock() => Faker = new Faker("pt_BR");
 
-        public Faker Faker {  get; set; }
+        public Faker Faker { get; set; }
     }
 }
